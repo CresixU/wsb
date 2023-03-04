b73809f4ced9c43acf27a4b94898394f03acf94c
kwawruch <cresixu@gmail.com>
2023-03-04 11:17:37 +0100

    Pierwszy zjazd

---
+++ 1_podstawy.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+$firstName = "Janusz";
+$lastName = "Nowak";
+echo "Imie i nazwisko $firstName $lastName";
+
+//heredoc
+$data = <<< DATA
+    <hr>
+    Imię: $firstName <br>
+    Nazwisko: $lastName <br>
+DATA;
+
+echo $data;
+
+//nowdoc
+echo <<< 'DATA'
+    <hr>
+    Imię: $firstName <br>
+    Nazwisko: $lastName <br>
+DATA;
+
+$bin = 0b1010;
+echo $bin;
+
+$oct = 010;
+echo $oct;
+
+$hex = 0xA;
+echo $hex;
+
+
+?>
+</body>
+</html>
---
+++ 2_html.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h4>Lista</h4>
+    <ul>
+        <li>Poznań</li>
+        <li>Łódź</li>
+        <li>Gniezno</li>
+    </ul>
+
+    <?php
+    echo <<< LIST
+    $city = "Środa wlkp."
+    <ul>
+        <li>Poznań</li>
+        <li>Łódź</li>
+        <li>Gniezno</li>
+        <li>$city</li>
+    </ul>
+    LIST;
+
+
+    require_once("scripts/script.php");
+
+    ?>
+</body>
+</html>
